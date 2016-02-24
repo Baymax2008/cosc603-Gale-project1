3ecdadb307c1cfeea6bc8f2ac20df5e6ac379841
Baymax2008 <Matt.John.Gale@gmail.com>
2016-02-24 02:39:54 -0500

    added Get functions for main

--- cosc603-Gale-project1/src/FireDanger.java
+++ cosc603-Gale-project1/src/FireDanger.java
@@ -1,6 +1,21 @@
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FireDanger.
+ */
 public class FireDanger {
 	
+	/**
+	 * Instantiates a new fire danger.
+	 *
+	 * @param dry the dry
+	 * @param wet the wet
+	 * @param iSnow the isnow
+	 * @param precip the precip
+	 * @param wind the wind
+	 * @param buo the buo
+	 * @param iHerb the iherb
+	 */
 	public FireDanger(double dry,    double wet,  boolean iSnow, 
 					  double precip, double wind, double buo,   int iHerb){
 		
@@ -44,7 +59,10 @@ public class FireDanger {
 	}
 	
 	
-	public static void calcFireDanger(){
+	/**
+	 * Calc fire danger.
+	 */
+	public void calcFireDanger(){
 		
 		double tempPrecip = mPrecip - .1;
 		
@@ -99,6 +117,37 @@ public class FireDanger {
 		}
 	}
 	
+	public double GetFLoad(){
+		return mFload;
+	}
+	
+	public double GetDF(){
+		return mDf;
+	}
+	
+	public double GetFFM(){
+		return mFfm;
+	}
+	
+	public double GetADFM(){
+		return mAdfm;
+	}
+	
+	public double GetGrass(){
+		return mGrass;
+	}
+	
+	public double GetTimber(){
+		return mTimber;
+	}
+	
+	public double GetBUI(){
+		return mBUI;
+	}
+	
+	/**
+	 * Calc fine fuel moisture.
+	 */
 	private static void CalcFineFuelMoisture(){
 		
 		double dif = mDry - mWet;
@@ -113,6 +162,10 @@ public class FireDanger {
 		mFfm = mB[i] * Math.exp(mA[i]);
 		
 	}
+	
+	/**
+	 * Calc draying factor.
+	 */
 	private static void CalcDrayingFactor(){
 		
 		int i = 0;
@@ -124,20 +177,41 @@ public class FireDanger {
 		mDf = i;
 		
 	}
+	
+	/**
+	 * Adjust fine fuel for herb stage.
+	 */
 	private static void AdjustFineFuelForHerbStage(){
 		
 		mFfm = mFfm + ( miHerb - 1 ) * 5;
 		
 	}
+	
+	/**
+	 * Inc bu iby drying factor.
+	 */
 	private static void IncBUIbyDryingFactor(){
 		mBUI = mBUI + mDf;
 		
 	}
+	
+	/**
+	 * Calc fuel adjusted fuel moist.
+	 */
 	private static void CalcFuelAdjustedFuelMoist(){
 		
 		mAdfm = .9 * mFfm + .5 + 9.5 * Math.exp(-mBUI/50);
 		
 	}
+	
+	/**
+	 * Caculate grass timber spread.
+	 *
+	 * @param doGrass the do grass
+	 * @param doTimber the do timber
+	 * @param a the a
+	 * @param b the b
+	 */
 	private static void caculateGrassTimberSpread(boolean doGrass, boolean doTimber,
 												  double a, double b){
 		if (doGrass){
@@ -159,6 +233,10 @@ public class FireDanger {
 			}
 		}
 	}
+	
+	/**
+	 * Calc fire load index.
+	 */
 	private static void CalcFireLoadIndex(){
 		mFload = 1.75 * Math.exp(mTimber) + .32 * Math.exp(mBUI) -1.640;
 		if (mFload < 0){
@@ -168,10 +246,20 @@ public class FireDanger {
 			mFload = Math.pow(10, mFload);
 		}
 	}
+	
+	/**
+	 * Change spread.
+	 *
+	 * @param index the index
+	 */
 	private static void ChangeSpread(double index){
 		mGrass = index;
 		mTimber = index;
 	}
+	
+	/**
+	 * Adjust bui.
+	 */
 	private static void AdjustBUI(){
 		mBUI = -50 * ( Math.log(1 - ( Math.exp( -mBUO / 50))) * Math.exp(1.175 * (mPrecip - .1)));
 		if (mBUI < 0 ){
@@ -180,24 +268,58 @@ public class FireDanger {
 		
 	}
 	
+	/** The m dry. */
 	private static double mDry;
+	
+	/** The m wet. */
 	private static double mWet;
+	
+	/** The mi snow. */
 	private static boolean miSnow;
+	
+	/** The m precip. */
 	private static double mPrecip;
+	
+	/** The m wind. */
 	private static double mWind;
+	
+	/** The m buo. */
 	private static double mBUO;
+	
+	/** The mi herb. */
 	private static int miHerb;
 	
+	/** The m df. */
 	private static double mDf;
+	
+	/** The m ffm. */
 	private static double mFfm;
+	
+	/** The m adfm. */
 	private static double mAdfm;
+	
+	/** The m grass. */
 	private static double mGrass;
+	
+	/** The m timber. */
 	private static double mTimber;
+	
+	/** The m fload. */
 	private static double mFload;
+	
+	/** The m bui. */
 	private static double mBUI;
+	
+	/** The m a. */
 	private static double[] mA = new double[4];
+	
+	/** The m b. */
 	private static double[] mB = new double[4];
+	
+	/** The m c. */
 	private static double[] mC = new double[3];
+	
+	/** The m d. */
 	private static double[] mD = new double[6];
 	
 			
